e05ea60aec3733786f51f82986b53f4b065f6bf7
Alex Padilla <alex.padilla@epn.edu.ec>
2023-08-30 00:31:45 -0500

    feat: update evaristo data

--- src/lib/images.lib.ts
+++ src/lib/images.lib.ts
@@ -1,6 +1,6 @@
 export const IMAGES = {
   CHAT: {
-    BACKGROUND: "/images/chat/bg.png",
+    BACKGROUND: "/images/chat/bg.svg",
     AI: "/images/avatar.png",
     DEFAULT: "/images/chat/default-avatar.png",
     DIALOGUE_BOX: "/images/chat/dialogue-box.png",
